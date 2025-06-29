1ac1898cfc90daf1ec174505f1a85323967e1ca1
akbar <akbaralaziz9@gmail.com>
2025-06-29 21:30:44 +0700

    coba

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
-// Import view
 import CreateForm from '../views/CreateForm.vue'
 
-const routes = [
-  {
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes : [
+    {
     path: '/',
     name: 'CreateForm',
     component: CreateForm
   },
-  // Tambahkan rute lain di sini jika perlu
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   component: () => import('../views/About.vue')
-  // }
-]
-
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  ]
 })
 
 export default router
